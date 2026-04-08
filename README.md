b6828b7a4a2b39f160b86b3753f509c73d4a04ac
Hossein Fakhari <fakhari.hossein@gmail.com>
2026-04-08 15:02:26 +0100

    move example directory to separate repo

--- README.md
+++
@@ -1,3 +0,0 @@
-cargo build --target wasm32-unknown-unknown
-
-wasm-pack build
--- crates/core/src/graph.rs
+++ crates/core/src/graph.rs
@@ -13,19 +13,19 @@ impl<VId, E, V> Graph<VId, E, V> {
         }
     }
 
-    pub fn add_vertex(&mut self, vid: VId, vertex: V) {
-        self.vertices.insert(vid, vertex);
-    }
+    // pub fn add_vertex(&mut self, vid: VId, vertex: V) {
+    //     self.vertices.insert(vid, vertex);
+    // }
+    //
+    // pub fn add_edge(&mut self, from: VId, to: VId, value: E) {
+    //     self.edges.entry(from).or_default().push((to, value));
+    // }
 
-    pub fn add_edge(&mut self, from: VId, to: VId, value: E) {
-        self.edges.entry(from).or_default().push((to, value));
-    }
-
-    pub fn get_vertex(&self, id: &VId) -> Option<&V> {
-        self.vertices.get(id)
-    }
-
-    pub fn get_edges(&self, id: &VId) -> Option<&Vec<(VId, E)>> {
-        self.edges.get(id)
-    }
+    // pub fn get_vertex(&self, id: &VId) -> Option<&V> {
+    //     self.vertices.get(id)
+    // }
+    //
+    // pub fn get_edges(&self, id: &VId) -> Option<&Vec<(VId, E)>> {
+    //     self.edges.get(id)
+    // }
 }
